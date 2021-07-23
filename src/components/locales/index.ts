6b6b14878d5e7649a620f19bf11976d66aaa98a6
刘百灵 <liubailing@skieer.com>
2021-07-23 18:25:09 +0800

    同步八爪鱼项目代码

---
+++ src/components/locales/index.ts
@@ -0,0 +1,19 @@
+export default {
+    FCEntities: {
+        Condition: "Condition",
+        ExtractData: "ExtractData",
+        Complete: "Complete",
+        EnterText: "EnterText",
+        Loop: "Loop",
+        BreakActivity: "BreakActivity",
+        Click: "Click",
+        MouseOver: "MouseOver",
+        Navigate: "Navigate",
+        SwitchCombo: "SwitchCombo",
+        EnterCapacha: "EnterCapacha",
+        Branch: "Branch",
+        WFGuideNode: "WFGuideNode",
+        WFGuideLine: "WFGuideLine",
+        WFGuideBranch: "WFGuideBranch"
+    }
+}
